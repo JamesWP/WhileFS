639c2501388c2cbf5aab2df4bfeb890314ffcd28
JamesWP <james_peach01@hotmail.co.uk>
2014-03-30 03:57:02 +0100

    finished launguage execution

--- WhileFS/WhileFS/Program.fs
+++ WhileFS/WhileFS/Program.fs
@@ -16,21 +16,21 @@ and Context =
         then this.variables.Item(i.Name)
         else failwith ("Invalid Identifier name: " + i.Name)
 
-type Statement = 
-    | Assignment
-    | Skip
-    | StatementConcat
-    | If
-    | While
+
 
 
 type ArithExp = 
-    | Var
-    | Int
-    | Addition
-    | Subtraction
-    | Multiply
-    member this.Value(c:Context) = 1 //TODO: stub
+    | Var of Identifier
+    | Int of int
+    | Addition of ArithExp * ArithExp
+    | Subtraction of ArithExp * ArithExp
+    | Multiply of ArithExp * ArithExp
+    member this.Value(c:Context) = match this with
+        | Var(i) -> c.ValueOf(i)
+        | Int(i) -> i
+        | Addition(a,b)-> a.Value c + b.Value c
+        | Subtraction(a,b) -> a.Value c - b.Value c
+        | Multiply(a,b) -> a.Value c * b.Value c
 
 type BooleanExp = 
     | True
@@ -44,28 +44,40 @@ type BooleanExp =
         | False -> false
         | Equals(a,b) -> a.Value c = b.Value c
         | LTEquals(a,b) -> a.Value c <=b.Value c
-        | Not(b) -> !b.Value
-        | 
+        | Not(a) -> not(a.Value c)
+        | And(a,b) -> a.Value c && b.Value c
 
 
-type Assignment = Identifier * ArithExp
-type StatementConcat = Statement * Statement
-type If(b:BooleanExp,strue:Statement,sfalse:Statement) = 
-    let b = b
-    let strue = strue
-    let sfalse = sfalse
+type Statement = 
+    | Assignment of Identifier * ArithExp
+    | Skip
+    | StatementConcat of Statement * Statement
+    | If of BooleanExp * Statement * Statement
+    | While of BooleanExp * Statement
+    member this.Compute(c:Context) = match this with
+        | Assignment(i,a) -> Context(c,i,a.Value c)
+        | Skip -> c
+        | StatementConcat(s1,s2) -> s1.Compute c |> s2.Compute
+        | If(b,strue,sfalse) -> if b.Value(c) then strue.Compute c else sfalse.Compute c
+        | While(b,s) -> 
+            let mutable cur = c
+            while (b.Value cur) do
+                cur <- s.Compute cur
+            cur
 
-let eq = Equals (True,True)
 
-
-type While(b:BooleanExp,s:Statement) = 
-    let b = b
-    let s = s
-
-
-
-
+let c = Context(Context(Context(),Identifier "x",1),Identifier "y",10)
+let prog = While(
+    LTEquals(Int 0,Var (Identifier "y")),
+    StatementConcat(
+        Assignment(Identifier "x",Multiply(Int 2,Var (Identifier "x"))),
+        Assignment(Identifier "y",Subtraction(Var (Identifier "y"),Int 1))
+    )
+)
+open System.Diagnostics
 [<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+let main argv =
+
+    let result = prog.Compute c
+    //Debug.WriteLine(result)
+    1 // return an integer exit code
